29554b8ea835654588a3e796734bf9cb820fdaf5
salmanulhamdan <salmanulhamdan106@gmail.com>
2023-07-24 12:47:34 +0530

    test

--- product/models.py
+++ product/models.py
@@ -43,7 +43,7 @@ class Product(models.Model):
         return self.name
     
 
-    
+# product varient model  
 
 class ProductVariant(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
