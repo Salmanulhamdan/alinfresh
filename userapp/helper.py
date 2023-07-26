9eb2209aa1f68848f712c9993ca38b97fe1244f4
salmanulhamdan <salmanulhamdan106@gmail.com>
2023-07-26 15:37:43 +0530

    changes in helper

--- userapp/helper.py
+++ userapp/helper.py
@@ -1,22 +1,11 @@
-# from django.conf import settings
-# from twilio.rest import Client
-# import random
-
-
-# class MessageHandler:
-#     phone_number=None
-#     otp=None
-#     def __init__(self,phone_number,otp) -> None:
-#         self.phone_number=phone_number
-#         self.otp=otp
-#     def send(self):     
-#         client= Client(settings.ACCOUNT_SID,settings.AUTH_TOKEN)
-#         message=client.messages.create (body=f'your otp is:{self.otp}',from_=f'{settings.TWILIO_PHONE_NUMBER}',to=f'{settings.COUNTRY_CODE}{self.phone_number}')
 
 
 import os
+from dotenv import load_dotenv
 from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
+# Load environment variables from .env file
+load_dotenv()
 
 client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
 verify = client.verify.services(os.environ['TWILIO_VERIFY_SERVICE_SID'])
